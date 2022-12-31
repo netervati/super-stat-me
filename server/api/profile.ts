c954f87407f07d5e9f3baa2fe10953181552a87d
netervati <netervati@gmail.com>
2023-01-01 03:56:19 +0800

    Create profile api and middleware

---
+++ server/api/profile.ts
@@ -0,0 +1,40 @@
+import { Octokit } from "octokit";
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  const runtimeConfig = useRuntimeConfig();
+
+  const octokit = new Octokit({
+    auth: runtimeConfig.personalAccessToken,
+  });
+
+  const {
+    data: {
+      avatar_url,
+      bio,
+      followers,
+      following,
+      login,
+      name,
+    },
+  } = await octokit.request('GET /users/{username}', {
+    username: String(query.username),
+  });
+
+  if (login !== query.username) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'User Not Found'
+    });
+  }
+
+  return {
+    avatar_url,
+    bio,
+    followers,
+    following,
+    login,
+    name,
+  };
+});
+
