dbe18f40d3f5b04deb174978fb98d6104c9c8bff
netervati <netervati@gmail.com>
2022-12-31 20:43:55 +0800

    Create landing page

---
+++ components/form/SearchField.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  },
+});
+</script>
+
+
+<template>
+  <input
+    @input="$emit('update:modelValue', $event.target.value)"
+    @keyup="$emit('onChange', $event.target.value)"
+    @keyup.enter="$emit('onEnter', $event.target.value)"
+    @value="modelValue"
+    class="
+      bg-slate-700
+      border-0
+      outline-0
+      h-12
+      rounded-lg
+      text-2xl
+      text-center
+      text-white
+    "
+  />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const searchUser = ref('');
+const isBlank = ref(false);
+
+const handleChange = (value: string): void => {
+  if (value !== "") {
+    isBlank.value = false;
+  }
+};
+
+const handleEnter = (value: string): void => {
+  if (value === "") {
+    isBlank.value = true;
+
+    return;
+  }
+
+  alert(value);
+};
+</script>
+
+<template>
+  <div class="flex h-screen w-screen">
+    <section class="m-auto">
+      <h3 class="mb-4 text-2xl text-center text-white">
+        Your GitHub username:
+      </h3>
+      <FormSearchField
+        @onChange="handleChange"
+        @onEnter="handleEnter"
+        v-model="searchUser"
+      />
+      <p
+        class="mt-2 text-slate-400"
+        v-if="isBlank"
+      >
+        Please input a username.
+      </p>
+    </section>
+  </div>
+</template>
