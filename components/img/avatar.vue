8ebe1bdaa3cb037d0bc5bcd970a9033f93f03847
netervati <netervati@gmail.com>
2023-01-01 16:20:20 +0800

    Create avatar component

---
+++ components/img/avatar.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+// @ts-ignore
+import VLazyImage from "v-lazy-image";
+
+defineProps({
+  avatar_url: {
+    required: true,
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <VLazyImage
+    :src="avatar_url"
+    :src_placeholder="avatar_url"
+    class="
+      -mt-20
+      absolute
+      border-8
+      border-white
+      left-0
+      ml-auto
+      mr-auto
+      right-0
+      rounded-full
+    "
+  />
+</template>
+
+<style scoped>
+.v-lazy-image {
+  filter: blur(10px);
+  transition: filter 0.7s;
+}
+
+.v-lazy-image-loaded {
+  filter: blur(0);
+}
+</style>
